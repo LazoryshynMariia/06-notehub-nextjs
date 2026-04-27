bea4648ac24e5fce03992ab64dac3e9f3381d8af
Mariia <ledimoon21@gmail.com>
2026-04-27 03:03:57 +0300

    moving the types folder

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { Note } from "./types/note";
+import type { Note } from "../types/note";
 
 axios.defaults.baseURL = "https://notehub-public.goit.study/api";
 axios.defaults.headers.common["Authorization"] = `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`;
