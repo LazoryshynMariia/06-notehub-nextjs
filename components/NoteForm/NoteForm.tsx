4aeed8b9c621f41fd2f83ad2a9de6e5bbba235ec
Mariia <ledimoon21@gmail.com>
2026-04-27 03:51:33 +0300

    fix NoteForm.tsx

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -17,7 +17,7 @@ const initialValues: NoteFormValues = {
     tag: "",
 }
 
-const NoteFormShema = Yup.object().shape({
+const NoteFormSchema = Yup.object().shape({
     title: Yup.string()
         .min(3, "Title must be at least 3 characters")
         .max(50, "Title is too long")
@@ -46,25 +46,25 @@ export default function NoteForm({onClose}: NoteFormProps) {
         }
     });
 
-    const handleSabmit = (
+    const handleSubmit = (
         values: NoteFormValues,
     ) => {
         mutation.mutate(values);
     };
 
     return (
-        <Formik initialValues={initialValues} validationSchema={NoteFormShema} onSubmit={handleSabmit}>
+        <Formik initialValues={initialValues} validationSchema={NoteFormSchema} onSubmit={handleSubmit}>
             {({isSubmitting}) => (<Form className={css.form}>
                 <div className={css.formGroup}>
-                    <label htmlFor={`${fieldId} - title`}>Title</label>
-                    <Field id={`${fieldId} - title`} type="text" name="title" className={css.input} />
+                    <label htmlFor={`${fieldId}-title`}>Title</label>
+                    <Field id={`${fieldId}-title`} type="text" name="title" className={css.input} />
                     <ErrorMessage component="span" name="title" className={css.error} />
                 </div>
 
                 <div className={css.formGroup}>
-                    <label htmlFor={`${fieldId} - content`}>Content</label>
+                    <label htmlFor={`${fieldId}-content`}>Content</label>
                     <Field as="textarea"
-                        id={`${fieldId} - content`}
+                        id={`${fieldId}-content`}
                         name="content"
                         rows={8}
                         className={css.textarea}
@@ -73,9 +73,9 @@ export default function NoteForm({onClose}: NoteFormProps) {
                 </div>
 
                 <div className={css.formGroup}>
-                    <label htmlFor={`${fieldId} - tag`}>Tag</label>
-                    <Field as="select" id={`${fieldId} - tag`} name="tag" className={css.select}>
-                        <option value="" disabled> Виберіть тег </option>
+                    <label htmlFor={`${fieldId}-tag`}>Tag</label>
+                    <Field as="select" id={`${fieldId}-tag`} name="tag" className={css.select}>
+                        <option value="" disabled> Select a tag </option>
                         <option value="Todo">Todo</option>
                         <option value="Work">Work</option>
                         <option value="Personal">Personal</option>
